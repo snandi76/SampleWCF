3f61625ef0bf801c7b5050756f8b66218afb7eb7
snandi76 <sunandan.nandi@gmail.com>
2018-12-30 18:00:22 +0530

    Implemented multiple contract using single wcf service and then exposes then using Http and tcp endpoint, so one http one can be accessed outside filewall and tcp can be accessed inside firewall.

---
+++ CompanyService/CompanyService/CompanyProvidedService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace CompanyService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CompanyProvidedService" in both code and config file together.
+    public class CompanyProvidedService : ICompanyProvidedPublicService, ICompanyProvidedConfidentialService
+    {
+        public string CompanyConfidentialService()
+        {
+            return "This is Company Confidential service inside firewall";
+        }
+
+        public string CompanyPublicService()
+        {
+            return "This is Company Publilc service outside firewall";
+        }
+    }
+}
---
+++ CompanyService/CompanyService/ICompanyProvidedService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace CompanyService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ICompanyProvidedService" in both code and config file together.
+    [ServiceContract]
+    public interface ICompanyProvidedPublicService
+    {
+        [OperationContract]
+        string CompanyPublicService();
+    }
+
+    [ServiceContract]
+    public interface ICompanyProvidedConfidentialService
+    {
+        [OperationContract]
+        string CompanyConfidentialService();
+    }
+}

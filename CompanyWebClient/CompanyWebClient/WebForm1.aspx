554b227fb550867621bba1adab6d3ee7e6d024b2
snandi76 <sunandan.nandi@gmail.com>
2018-12-30 18:06:37 +0530

    Webclient for the Company multiple contarct  exposed through same service project project. Can be used as a client with "CompanyService" project.

---
+++ CompanyWebClient/CompanyWebClient/WebForm1.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="CompanyWebClient.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <table style="font-family:'Times New Roman', Times, serif">
+        <tr>
+            <td>
+                <asp:Button ID="Button1" runat="server" Text="PublicInfo" OnClick="Button1_Click" /></td>
+            <td>
+                <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label></td>
+        </tr>
+        <tr>
+            <td>
+                <asp:Button ID="Button2" runat="server" Text="PrivateInfo" OnClick="Button2_Click" />
+            </td>
+            <td>
+                <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+            </td>
+        </tr>
+    </table>
+    </div>
+    </form>
+</body>
+</html>
---
+++ CompanyWebClient/CompanyWebClient/WebForm1.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.ComponentModel;
+
+namespace CompanyWebClient
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            CompanyService.CompanyProvidedPublicServiceClient PubClient = new CompanyService.CompanyProvidedPublicServiceClient("BasicHttpBinding_ICompanyProvidedPublicService");
+            Label1.Text = PubClient.CompanyPublicService();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            CompanyService.CompanyProvidedConfidentialServiceClient ConClient = new CompanyService.CompanyProvidedConfidentialServiceClient("NetTcpBinding_ICompanyProvidedConfidentialService");
+            Label2.Text = ConClient.CompanyConfidentialService();
+        }
+    }
+}
